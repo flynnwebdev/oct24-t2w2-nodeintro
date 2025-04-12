5ef68387ea488b45f26a1276fac7a5eca1c87795
Mr. Robot <flynnwebdev@gmail.com>
2025-04-12 13:28:49 +1000

    Started Posts routes

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express') // CommonJS modules
 const app = express()
 const port = 3000
 
+const person = {
+    name: 'Matt',
+    age: 52
+}
+
+const posts = [
+    {
+        title: 'Post 1',
+        body: 'Lorem ipsum dolor sit amet',
+        isPublished: false
+    },
+    {
+        title: 'Post 2',
+        body: 'This is the body of post 2',
+        isPublished: true
+    }
+]
+
 // Python/Flask:
 // @app.route('/', methods=['GET']) // default get
 // def home():
@@ -10,11 +28,24 @@ const port = 3000
 // app.[http-verb]([path], [callback])
 // Callback accepts a request and response object
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    // Serialize person and send to the client
+    res.send(person)
 })
+
+// Posts routes
+
+// Get all posts
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+// Get one post
+// Create a new post
+// Update a post
+// Delete a post
+
 
 // Start the dev server on the given port
 // The callback is called when the server is running
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
